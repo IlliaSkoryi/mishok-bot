66534bafaba9aed587eeb12986eb6f9b7375b605
Illia Skoryi <skorillia@gmail.com>
2018-10-05 21:58:42 +0300

    Initial commit

---
+++ src/main/java/com/lsoft/mishoksubstitute/MishoksubstituteApplication.java
@@ -0,0 +1,12 @@
+package com.lsoft.mishoksubstitute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MishoksubstituteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MishoksubstituteApplication.class, args);
+	}
+}
